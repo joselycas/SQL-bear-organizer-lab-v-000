9939b9e2974b7a45d1dedf514a8073446de7bdd7
Jos Castro <joselycas@gmail.com>
2018-08-03 19:15:40 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   #{}"SELECT COUNT(color) FROM bears WHERE color "
-  "SELECT color, COUNT(color) FROM bears GROUP BY color ASC LIMIT 1"
+  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY color ASC LIMIT 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
